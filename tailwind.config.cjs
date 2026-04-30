c5af41a411678fc216b1b841cc3997fd1dc7d5ce
CodeAndrew17 <becerrafelipe8b@gmail.com>
2026-04-30 18:01:23 -0500

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gradientDarkStart: '#004466',
+        gradientDarkEnd: '#000435',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+};
